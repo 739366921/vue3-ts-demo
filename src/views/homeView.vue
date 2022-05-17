5549b42158edfb3917cb11eadfc11b8bff128dd1
wwwjjjyyy <739366921@qq.com>
2022-05-17 16:59:38 +0800

    feat：添加逻辑

--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -6,7 +6,26 @@
     <dropdown-menu>
       <dropdown-item v-model="value" :options="option" @change="changeMenu"/>
     </dropdown-menu>
-    
+    <div class="info-container">
+      <List
+          v-model:loading="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="onLoad"
+      >
+        <Row>
+          <Col span="8" v-for="item in list" :key="item">
+            <Image
+                width="100"
+                height="100"
+                lazy-load
+                src="https://cdn.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+            />
+          </Col>
+        </Row>
+      </List>
+
+    </div>
   </div>
 </template>
 
@@ -15,14 +34,18 @@ import dayjs from "dayjs";
 import {defineComponent, ref} from "vue";
 // import VideoInfoApi from "/src/api/apis";
 import {getVideoInfo} from "@/api/apis";
-import {Search, DropdownMenu, DropdownItem} from "vant";
+import {Search, DropdownMenu, DropdownItem, Row, Col, List, Image as VanImage} from "vant";
 
 export default defineComponent({
   name: "HomePgae",
   components: {
     Search,
     DropdownMenu,
-    DropdownItem
+    DropdownItem,
+    Col,
+    Row,
+    List,
+    Image
   },
   data() {
     return {
@@ -40,39 +63,52 @@ export default defineComponent({
       this.timer = setInterval(() => {
         this.time = dayjs().format("YYYY-MM-DD HH:mm:ss");
       }, 1000);
-    },
-    changeMenu() {
-      this.filter.key = ''
     }
   },
   setup() {
-    const value = ref('title');
-    const filter={
+    const list = ref([])
+    const loading = ref(false)
+    const finished = ref(false)
+    const value = ref('title')
+    const filter = ref({
       option: 'title',
       key: '',
       current: 1,
       size: 10
-    };
-    const option = [
+    })
+    const option = ref([
       {text: '标题名称', value: 'title'},
       {text: '导演名称', value: 'director'},
       {text: '主演名称', value: 'actor'},
       {text: '地区名称', value: 'region'},
       {text: '上映时间', value: 'releaseTime'},
       {text: '分类名称', value: 'videoType'},
-    ];
-  const getVideo= async function getVideoImage() {
-      const res = await getVideoInfo(filter)
-      console.log('res', res)
+    ])
+    const getVideo = async () => {
+     const res = await getVideoInfo(filter.value)
+      console.log(res.value)
+      list.value.push(res.value)
+    };
+    const changeMenu = () => {
+      filter.value.key = ''
     };
     const onSearch = () => {
       getVideo()
     };
+    const onLoad = () => {
+      filter.value.current += 1
+      getVideo()
+    };
     return {
+      list,
+      loading,
+      finished,
       value,
       option,
       filter,
-      onSearch
+      onSearch,
+      onLoad,
+      changeMenu
     }
   },
   created() {
@@ -86,7 +122,7 @@ export default defineComponent({
 
 <style scoped>
 .home {
-  --van-dropdown-menu-height:0.66rem;
-  --van-dropdown-menu-title-font-size:13px;
+  --van-dropdown-menu-height: 0.66rem;
+  --van-dropdown-menu-title-font-size: 13px;
 }
 </style>
